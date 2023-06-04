931c4739836520f12bfbfc9b43b8e245a49fec43
Clyde McQueen <clyde@mcqueen.net>
2023-06-04 11:01:26 -0700

    Enable rangefinder logging

--- ArduSub/sensors.cpp
+++ ArduSub/sensors.cpp
@@ -18,6 +18,7 @@ void Sub::read_barometer()
 void Sub::init_rangefinder()
 {
 #if RANGEFINDER_ENABLED == ENABLED
+    rangefinder.set_log_rfnd_bit(MASK_LOG_CTUN);
     rangefinder.init(ROTATION_PITCH_270);
     rangefinder_state.alt_cm_filt.set_cutoff_frequency(RANGEFINDER_WPNAV_FILT_HZ);
     rangefinder_state.enabled = rangefinder.has_orientation(ROTATION_PITCH_270);
